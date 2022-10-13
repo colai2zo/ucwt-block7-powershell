4ae450ddf3809944222728ac820810ca07b78121
colai2zo <joeycolaizzo@gmail.com>
2022-10-12 20:10:13 -0500

    Added day 6 worksheet.

---
+++ day6a.ps1
@@ -0,0 +1,59 @@
+﻿<#
+    2d Lt Joseph H. Colaizzo
+    Day 6 Worksheet A
+#>
+
+# Problem 2
+$a = 5
+$b = "Name"
+$c = @()
+
+# Problem 3
+function main ($a, $b, $c) {
+    $number = Read-Host "Please input a number"
+    if ($a -eq $number) {
+        "`$a and `$number are the same."
+    } else {
+        "`$a and `$number are different."
+    }
+
+    #Problem 4
+    $c += "name"
+    do {
+        $c += Read-Host "Please enter a name"
+    } until ($c.Length -eq 5)
+
+    # Problem 5
+    forEach ($name in $c) {
+        if ($name.Length -gt 5) {
+            $name >> LongName.txt
+        } else {
+            $name >> ShortName.txt
+        }
+    }
+}
+
+function PrintTextFile {
+    try {
+        cat LongName.txt
+    } catch {
+        "Unable to read LongName.txt or does not exist."
+    }
+    try {
+        cat ShortName.txt
+    } catch {
+        "Unable to read ShortName.txt or does not exist."
+    }
+}
+
+$null > LongName.txt
+$null > ShortName.txt
+main $a $b $c
+PrintTextFile
+
+$profile = @{Hometown = "Chicago"; Height = 60; Weight = 150; Name = "Fred"}
+$profile.Age = 39
+$profile.Weight = 200
+$profile.Hometown = Read-Host "Enter your hometown"
+$profile
+$profile.Count
---
+++ day6b.ps1
@@ -0,0 +1,41 @@
+﻿<#
+    2d Lt Joseph H. Colaizzo
+    Day 6 Worksheet B
+#>
+
+function printFiles {
+    cat High.txt
+    cat Low.txt
+}
+
+function highLow ($evens, $name) {
+    forEach ($even in $evens) {
+        if ($even -ge 50) {
+            $even >> High.txt
+        } else {
+            $even >> Low.txt
+        }
+    }
+}
+
+function numInput ($name) {
+    $evens = @()
+    do{
+        [int] $num = Read-Host "Please enter an even number"
+        if ($num % 2) { # Odd
+            "Please try again (only even numbers accepted)."
+        } else {
+            $evens += $num
+        }
+    } until ($evens.Length -eq 5)
+    highLow $evens $name
+}
+
+
+function userName {
+    $name = Read-Host "Please enter your name"
+    numInput $name
+}
+
+userName
+printFiles
