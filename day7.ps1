0bda795be51801671e10d998b60026c51562ee10
colai2zo <joeycolaizzo@gmail.com>
2022-10-12 20:20:31 -0500

    Added day 7

---
+++ day7.ps1
@@ -0,0 +1,32 @@
+﻿<# 
+    2d Lt Joseph H. Colaizzo
+    Day 7 Worksheet
+#>
+
+$FootballTeam = @{City = "Indianapolis"; State = "Indiana"; Country = "United States"; Mascot = "Colts"}
+$FootballTeam.Add("Best Player", "Peyton Manning")
+$FootballTeam.City = "Baltimore"
+
+function WriteToFile ($Cities) {
+    "Cities" > cities.txt
+    $Cities >> cities.txt
+    cat cities.txt
+}
+
+function AskUser {
+    $Cities = @()
+    do {
+        $Cities += Read-Host "Enter a city you would like to explore"
+    } until ($Cities.Length -eq 8)
+    $Cities
+    WriteToFile $Cities
+}
+
+function main ($TeamInfo) {
+    $TeamInfo.Mascot = Read-Host "Enter a new mascot"
+    AskUser
+}
+
+main $FootballTeam
+
+
